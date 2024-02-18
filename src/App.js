b631f43dfbd48ddc0cc39a46483f7f0b90c21474
Katheryn Koay <kykoay97@gmail.com>
2024-02-18 14:12:48 +0800

    push

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React from "react";
-import { Provider } from "react-redux";
 import AddressSelection from "./component/AddressSelection";
-import store from "./redux/store";
 
 const App = () => {
   console.log("App component rendering...");
 
-  store.dispatch({ type: "SET_SEARCH_TERM", payload: "Test Search Term" });
-  store.dispatch({
-    type: "ADD_SEARCH_HISTORY",
-    payload: "",
-  });
-
-  return (
-    <Provider store={store}>
-      <AddressSelection />
-    </Provider>
-  );
+  return <AddressSelection />;
 };
 
 export default App;
